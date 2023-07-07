872145cdf69b37b33365b5ffc06fe2b7b35d3fc9
essey1 <esseyt3@gmail.com>
2023-07-07 10:41:44 +0300

    added upload.php file which will move the uploaded file to the books directory

---
+++ upload.php
@@ -0,0 +1,5 @@
+<?php
+
+// Moves uploaded file to a books directory
+$targetPath = "books/" . basename($_FILES["inpFile"]["name"]):
+move_uploaded_file($_FILES["inpFile"]["tmp_name"]. $targetPath):
